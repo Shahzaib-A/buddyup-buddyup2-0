12a1eeb63d60d9e084c2a6682ebaf4629f479510
743627 <54897138+743627@users.noreply.github.com>
2021-02-25 12:52:30 -0500

    Made it so sign up and sign in are on one html file

--- registerSign.js
+++ registerSign.js
@@ -1,3 +1,13 @@
+//display sign up
+document.querySelector('.create').addEventListener('click',function(){
+  document.querySelector('.signin').style.display = 'none';
+  document.querySelector('.signup').style.display = 'block';
+})
+//display sign in
+document.querySelector('.back').addEventListener('click',function(){
+  document.querySelector('.signin').style.display = 'block';
+  document.querySelector('.signup').style.display = 'none';
+})
 
 //variables
 const original_password = document.querySelector('.password');
