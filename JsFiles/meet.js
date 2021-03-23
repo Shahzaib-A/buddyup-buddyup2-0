7cc67f56afe19b57fba05384491c942aa40c6434
743627 <54897138+743627@users.noreply.github.com>
2021-03-23 16:52:12 -0400

    Chat

--- JsFiles/meet.js
+++ JsFiles/meet.js
@@ -1,3 +1,7 @@
 $(".send").click(function(){
-  $(".enter-message").val('')
+  x = firebase.database().ref('/Users/MWVM7KWyTEC8YEiT2JI/name')
+  alert(x)
+  let messageObject = {
+      message: $(".enter-message").val()
+  }
 })
