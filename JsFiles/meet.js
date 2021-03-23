d2ffd80d6c49de43d486d1bc758a227295d5e82a
743627 <54897138+743627@users.noreply.github.com>
2021-03-23 15:06:31 -0400

    Link

--- JsFiles/meet.js
+++ JsFiles/meet.js
@@ -1,24 +1,6 @@
-var isHidden = true
-$(".icon").click(function(){
-    switch (this.id){
-      case "dm":
-        $(".friendRq").fadeOut();
-        isHidden= false
-        break;
-      case "public":
-        if(!isHidden){
-          $(".friendRq").hide().fadeIn()
-          isHidden = true
-        }
-        break;
-      case "add":
-        alert("add contacts")
-        break;
-    }
-})
-
 $(".send").click(function(){
-  let messageToSend = document.querySelector('.text').value.toString()
-  let sender = auth.currentUser.uid.toString()
-  proccesMessage(messageToSend,sender)
+  alert()
+  // let messageObject = {
+  //   sender:
+  // }
 })
