79d02a6111e2ef8403a4fa45cacaec683c159019
743627 <54897138+743627@users.noreply.github.com>
2021-04-08 10:17:52 -0400

    Finished Calendar
    The actual calendar code html and css is Harsh, I just fixed layout

--- JsFiles/nav.js
+++ JsFiles/nav.js
@@ -1,11 +1,3 @@
-let open = false
 $("#navs").click(() =>{
-	if(open){
-			$("nav ul").css("width", "0%");
-			open = false;
-	}
-	else{
-		$("nav ul").css("width", "35%");
-		open = true;
-	}
+	$("nav ul").toggle()
 })
